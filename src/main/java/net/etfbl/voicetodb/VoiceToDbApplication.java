18f6cad55b417e970ccf985dda7719a1aced9111
Chardash <nebojsa.baabic@gmail.com>
2021-03-09 20:40:27 +0100

    Setup base spring structure

---
+++ src/main/java/net/etfbl/voicetodb/VoiceToDbApplication.java
@@ -0,0 +1,13 @@
+package net.etfbl.voicetodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceToDbApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(VoiceToDbApplication.class, args);
+   }
+
+}
