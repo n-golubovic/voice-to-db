91aa5db08e7d509ac88c8b4e359e9c10b9dcb8af
Nenad Golubovic <n.golubovic1994@gmail.com>
2021-03-27 20:43:41 +0100

    Add PythonRunner

---
+++ src/main/java/net/etfbl/voicetodb/components/PythonRunner.java
@@ -0,0 +1,42 @@
+package net.etfbl.voicetodb.components;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PythonRunner {
+
+   private final String scriptPath;
+   private final ObjectMapper objectMapper;
+
+   @Autowired
+   public PythonRunner(@Value("${voice-to-db.vosk.script-path}") String scriptPath, ObjectMapper objectMapper) {
+      this.scriptPath = scriptPath;
+      this.objectMapper = objectMapper;
+   }
+
+   public List<String> runAndListenScript(String fileName) throws IOException {
+      ProcessBuilder builder = new ProcessBuilder();
+      builder.command("python.exe", "test_simple2.py", fileName);
+      builder.directory(new File(scriptPath));
+      Process process = builder.start();
+
+      List<String> result = objectMapper.readValue(process.getInputStream(), RecognitionResult.class);
+      result.removeIf(String::isBlank);
+
+      return result;
+   }
+
+   static class RecognitionResult extends ArrayList<String> {
+   }
+
+}
+
+
