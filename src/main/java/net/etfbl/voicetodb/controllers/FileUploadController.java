ad19642cbe99a1bf9e70cd38b6750d45ab06089b
Chardash <nebojsa.baabic@gmail.com>
2021-04-04 21:38:31 +0200

    Fix server error on uploading empty or non-audio only files

--- src/main/java/net/etfbl/voicetodb/controllers/FileUploadController.java
+++ src/main/java/net/etfbl/voicetodb/controllers/FileUploadController.java
@@ -1,6 +1,9 @@
 package net.etfbl.voicetodb.controllers;
 
 
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import net.etfbl.voicetodb.models.FileUploadResponse;
 import net.etfbl.voicetodb.services.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 public class FileUploadController {
@@ -22,7 +23,21 @@ public class FileUploadController {
    }
 
    @PostMapping("/upload")
-   public FileUploadResponse uploadFiles(@RequestParam("files") List<MultipartFile> files) {
+   public FileUploadResponse uploadFiles(HttpServletResponse response,
+                                         @RequestParam("files") List<MultipartFile> files) {
+      files.removeIf(MultipartFile::isEmpty);
+      files.removeIf(FileUploadController::notAudioFile);
+
+      if (files.isEmpty()) {
+         response.setStatus(SC_BAD_REQUEST);
+         return FileUploadResponse.EMPTY;
+      }
+
       return new FileUploadResponse(fileUploadService.save(files));
    }
+
+   private static boolean notAudioFile(MultipartFile file) {
+      return file.getContentType() != null && !file.getContentType().matches("audio/.*");
+   }
+
 }
--- src/main/java/net/etfbl/voicetodb/models/FileUploadResponse.java
+++ src/main/java/net/etfbl/voicetodb/models/FileUploadResponse.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class FileUploadResponse {
 
+   public static FileUploadResponse EMPTY = new FileUploadResponse(null);
+
    private final String id;
 
 }
