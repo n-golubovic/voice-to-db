ece2075eadf7fd0bbe649582c65c3e4efe4daa42
Nenad Golubovic <n.golubovic1994@gmail.com>
2021-04-16 18:12:48 +0200

    Add test for text processor

---
+++ src/test/java/net/etfbl/voicetodb/components/TextProcessorTest.java
@@ -0,0 +1,33 @@
+package net.etfbl.voicetodb.components;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TextProcessorTest {
+
+   TextProcessor textProcessor = new TextProcessor();
+
+   @Test
+   void process_validInput_returnsJoinedString() {
+      List<String> input = Arrays.asList("sentence 1", "sentence 2");
+
+      String result = textProcessor.process(input);
+
+      assertEquals("sentence 1. sentence 2", result);
+   }
+
+   @Test
+   void process_forEmptyList_returnsEmptyString() {
+      List<String> input = Collections.emptyList();
+
+      String result = textProcessor.process(input);
+
+      assertEquals("", result);
+   }
+
+}
